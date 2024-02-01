97aeb3d8b56503065304e7830e4c70bd8fab13da
Austin Moody <austin.moody@gmail.com>
2024-01-31 22:21:07 -0500

    Refactor code for pulling Oura token and enhance output messages
    The commit refactors the code pulling the Oura personal token from environment and relocates it to a function where it's primarily needed. Additional output messages have been added to give more clarity on what the program is processing, including a program banner, insertion confirmations, and more explicit error messages.

--- oura_puller/oura_puller.go
+++ oura_puller/oura_puller.go
@@ -3,20 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/joho/godotenv"
-	"log"
 	"os"
 	"time"
 )
 
 func main() {
 
-	// Pull Oura token from environmentd
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	ouraPersonalToken := os.Getenv("OURA_PERSONAL_TOKEN")
+	printBanner()
 
 	// Setup Command Line Arguments
 	var customFlag TypeChoices
@@ -27,6 +20,11 @@ func main() {
 	endDateInput := flag.String("end-date", time.Now().Format("2006-01-02"), "End date to search for Oura Ring data (defaults to today)")
 
 	flag.Parse()
+
+	if customFlag.Value == "" {
+		fmt.Println("You must specify Oura Ring data type")
+		os.Exit(1)
+	}
 
 	startDate, err := time.Parse("2006-01-02", *startDateInput)
 	if err != nil {
@@ -40,11 +38,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println("Type Specified: " + customFlag.Value)
+	fmt.Println("Processing Oura Ring Type: " + customFlag.Value)
 	fmt.Println("Start Date: " + startDate.Format("2006-01-02"))
 	fmt.Println("End Date: " + endDate.Format("2006-01-02"))
+	fmt.Println("--------------------------------------------")
 
 	if customFlag.Value == "sleep" {
-		ProcessSleep(ouraPersonalToken, startDate, endDate)
+		ProcessSleep(startDate, endDate)
 	}
+}
+
+func printBanner() {
+	banner := `
+ ___            _    _        ___  ___  ___
+/   \ _  _  ___| |_ (_) _ _  /   \| _ \|_ _|
+| - || || |(_-/|  _|| || ' \ | - ||  _/ | |
+|_|_| \_._|/__/ \__||_||_||_||_|_||_|  |___|
+
+AustinAPI Oura Ring Data Puller
+============================================
+`
+	fmt.Print(banner)
 }
--- oura_puller/sleep_data.go
+++ oura_puller/sleep_data.go
@@ -11,35 +11,46 @@ import (
 	"time"
 )
 
-func ProcessSleep(personalToken string, startDate time.Time, endDate time.Time) {
+func ProcessSleep(startDate time.Time, endDate time.Time) {
 
 	// TODO: Something to think about... specifying a start & end a day apart.
 	// so Start = 2024-01-29 & End = 2024-01-30
 	// Sleep will bring back 1 item dated 2024-01-29
 	// Daily Sleep will bring back 1 items: 2024-01-29 and 2024-01-30
 
+	// Pull Oura token from environment
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	ouraPersonalToken := os.Getenv("OURA_PERSONAL_TOKEN")
+
 	// Here we would use oura_go to pull sleep data and parse out what we need
-	client := go_oura.NewClient(personalToken)
+	client := go_oura.NewClient(ouraPersonalToken)
 
 	// Get Sleep from API, has information about durations & start/end time etc...
-	sleepDocs, err := client.GetSleeps(startDate, endDate, nil)
-	if err != nil {
-		fmt.Printf("Error getting Sleep Items: %v\n", err)
+	fmt.Printf("Pulling Sleep Items\n")
+	sleepDocs, ouraError := client.GetSleeps(startDate, endDate, nil)
+	if ouraError != nil {
+		fmt.Printf("Error getting Sleep Items: %v\n", ouraError)
 		return
 	}
 
 	for _, sleepDoc := range sleepDocs.Items {
 		InsertSleepData(sleepDoc.Day.Format("2006-01-02"), 0, sleepDoc.TotalSleepDuration)
+		fmt.Printf("\tSleep Item %s Inserted\n", sleepDoc.ID)
 	}
 
 	// Get Daily Sleep from API, has the Score
-	dailySleeps, err := client.GetDailySleeps(startDate, endDate, nil)
-	if err != nil {
-		fmt.Printf("Error getting Daily Sleep items: %v\n", err)
+	fmt.Printf("Pulling Daily Sleep Items\n")
+	dailySleeps, ouraError := client.GetDailySleeps(startDate, endDate, nil)
+	if ouraError != nil {
+		fmt.Printf("Error getting Daily Sleep items: %v\n", ouraError)
 	}
 
 	for _, dailySleep := range dailySleeps.Items {
 		InsertDailySleepData(dailySleep.Day.Format("2006-01-02"), dailySleep.Score)
+		fmt.Printf("\tDaily Sleep Item %s Inserted\n", dailySleep.ID)
 	}
 }
 
@@ -88,8 +99,6 @@ func InsertSleepData(date string, rating int, totalDuration int) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Sleep data successfully inserted!")
 }
 
 func InsertDailySleepData(date string, score int64) {
@@ -113,7 +122,4 @@ func InsertDailySleepData(date string, score int64) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Daily Sleep data successfully inserted!")
-
 }
